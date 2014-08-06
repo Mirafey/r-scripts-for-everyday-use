54f717412f5ab17d0726eed6b8e3332147a0138f
Danilo Freire <danilofreire@users.noreply.github.com>
2014-08-06 11:20:29 -0300

    Update counterfactuals.R

--- counterfactuals.R
+++ counterfactuals.R
@@ -1,30 +1,28 @@
-# What if? Software for Evaluating Counterfactuals (Stoll, King and Zeng, 2010)
+# "Whatif?": Software for Evaluating Counterfactuals (Stoll, King and Zeng, 2010)
 # http://gking.harvard.edu/whatif
 # http://gking.harvard.edu/files/abs/counterft-abs.shtml Read!
 # http://gking.harvard.edu/files/abs/counterf-abs.shtml Read too!
 
-# Not all counterfactuals can be evaluated. As the authors say:
-# "when the counterfactuals posed are too far from the data at hand, conclusions drawn
+# Not all counterfactuals should be evaluated. As the authors say:
+# "[...] when the counterfactuals posed are too far from the data at hand, conclusions drawn
 # from well-specified statistical analyses become based on speculation and convenient
 # but indefensible model assumptions rather than empirical evidence. Unfortunately,
 # standard statistical approaches assume the veracity of the model rather than 
 # revealing the degree of model-dependence, and so this problem can be hard to detect."
 
-# WhatIf is a package that helps to indentify those bad counterfactuals
-# with a matching software. It checks if the counterfactual data are in convex hull
+# WhatIf is a package that helps to identify such invalid counterfactuals
+# It checks if the counterfactual data are in convex hull
 
-# Let's show an example below:
-library(WhatIf) # the package itself
-library(MatchIt) # for matching (not necessary, just to test other things)
-library(Zelig) # for estimation
+# An example:
+library(WhatIf)  # the package itself
+library(Zelig)   # model estimation
 
 # Load the dataset
 data(turnout)
 turnout$race1 <- ifelse(turnout$race=="white",0, 1) # recode treatment as non-white
 
 # Estimate a simple model
-model1 <- zelig(vote ~ race1 + age + educate + income, data = turnout,
-                model = "logit")
+model1 <- zelig(vote ~ race1 + age + educate + income, data = turnout, model = "logit")
 summary(model1)
 
 # Evaluate a counterfactual where race = 0
@@ -46,11 +44,10 @@ plot(whif2, type = "b")
 summary(whif2)
 
 # Or more generally:
-# Create a new dataset with the quantities of interest of the 
-# covariates (no intercept!)
+# Create a new dataset with the quantities of interest of the covariates (no intercept!)
 summary(whatif(data = model1, cfact = data.frame(race1 = 1, 
                                                  age = mean(turnout$age),
                                                  educate = mean(turnout$educate),
                                                  income = 10*mean(turnout$income))))
 
-# The test correctly identified a bad counterfactual
+# The test correctly identified an improper counterfactual
